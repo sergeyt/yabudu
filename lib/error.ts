f3a04f9ec448c46e0345188ebdec2de03c53473b
Sergey Todyshev <stodyshev@gmail.com>
2025-12-01 21:46:27 +0300

    fix: build

--- lib/error.ts
+++ lib/error.ts
@@ -55,8 +55,8 @@ type Route<TParams extends ParamBase = any> = (
 
 export function errorMiddleware<TParams extends ParamBase = any>(
   fn: Route<TParams>,
-): Route<TParams> {
-  return async (req, ctxFromNext?: RouteContext<TParams>) => {
+): any {
+  return async (req: Request, ctxFromNext?: RouteContext<TParams>) => {
     try {
       let ctx = ctxFromNext;
       if (!ctxFromNext?.params) {
